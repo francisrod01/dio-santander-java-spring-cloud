d502a3233468f7f1f710d7fde564269f8dd2760a
Francis Rodrigues <francis.rod01@outlook.com>
2021-07-14 16:19:45 -0300

    feat(productCatalog) generated spring boot application

---
+++ product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/ProductCatalogApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.experts.productcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogApplication.class, args);
+	}
+
+}
