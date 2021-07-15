1972d8d63c906a90669d5d78a363c3db0036660c
Francis Rodrigues <francis.rod01@outlook.com>
2021-07-15 17:08:45 -0300

    feat(ProductCatalog): Added a product controller

---
+++ product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/controller/ProductController.java
@@ -0,0 +1,22 @@
+package one.digitalinnovation.experts.productcatalog.controller;
+
+import one.digitalinnovation.experts.productcatalog.model.Product;
+import one.digitalinnovation.experts.productcatalog.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/product")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @RequestMapping(method = RequestMethod.POST)
+    Product create(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+}
