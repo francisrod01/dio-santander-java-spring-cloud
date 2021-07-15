756725fdac7c9a9c823f72aa2377ecec348045c4
Francis Rodrigues <francis.rod01@outlook.com>
2021-07-15 17:09:24 -0300

    feat(ProductCatalog): Added an ElasticSearch config

---
+++ product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/config/ElasticSearchConfig.java
@@ -0,0 +1,22 @@
+package one.digitalinnovation.experts.productcatalog.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "one.digitalinnovation.experts.productcatalog.repository")
+public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+
+    @Override
+    public RestHighLevelClient elasticsearchClient() {
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo("localhost:9200", "localhost:9300")
+                .build();
+        return RestClients.create(clientConfiguration).rest();
+    }
+
+}
